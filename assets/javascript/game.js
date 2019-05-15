5edada528a3ba8822db5ed76e7f772f368cad3e8
Jamie Barton <jbarton10@email.arizona.edu>
2019-05-15 12:51:27 -0700

    On key up added to js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,13 @@
 var wordsToGuess = ["mario","donkeykong","link","samus","darksamus","yoshi","kirby","fox","pikachu","luigi","ness"];
+var charGuess;
+var word2Guess= wordsToGuess[Math.floor(Math.random()*wordsToGuess.length)];
+var charGuessed = [];
+
+
+
+
+document.onkeyup = function(event){
+    charGuess = event.key;
+    console.log(charGuess);
+
+}
