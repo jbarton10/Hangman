a03caa83f545efcb88dcf1768b91e13287f9ea43
Jamie Barton <jbarton10@email.arizona.edu>
2019-05-15 14:22:45 -0700

    More work done on js, added some catches so guesses cannot be repeated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,41 @@
 var wordsToGuess = ["mario","donkeykong","link","samus","darksamus","yoshi","kirby","fox","pikachu","luigi","ness"];
 var charGuess;
 var word2Guess= wordsToGuess[Math.floor(Math.random()*wordsToGuess.length)];
-var charGuessed = [];
+var charsGuessed = [];
+var guessesLeft = 10;
+
+//This should probably not inclued a space, otherwise I would think
+//Indexing will be hard later on
+var displayedWord="_ ".repeat(word2Guess.length);
+console.log(word2Guess);
+console.log(displayedWord);
 
 
 
 
 document.onkeyup = function(event){
     charGuess = event.key;
-    console.log(charGuess);
+    
+    //Makes sure that you cant select the same letter char twice
+    if(!charsGuessed.includes(charGuess)){
+        //Increments the guesses
+        charsGuessed.push(charGuess); 
+        guessesLeft--;
+        
+        //checks to see if you got it right
+        if(word2Guess.includes(charGuess)){
+            //Looking for the correct char in the str
+            for(var i = 0; i< word2Guess.length; i++){
+                //checks the char vs index
+                if(charGuess === word2Guess[i]){
+                    //Index into displayedWord to change it
+                    displayedWord[i]=charGuess;
+                    
+                }
+            }
+        guessesLeft--;
 
+        }
+
+    }
 }
